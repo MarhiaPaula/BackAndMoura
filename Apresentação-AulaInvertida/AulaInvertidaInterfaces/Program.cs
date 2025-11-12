84f8bf4107033ff41adce93a4f253ff0365277c6
Marhia Paula <marhiapaulamrq@gmail.com>
2025-11-12 13:29:46 -0300

    adicionei novos exercicios e a aula invertida

---
+++ Apresentação-AulaInvertida/AulaInvertidaInterfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿
+interface IAnimal //Define o "contrato" (métodos obrigatórios) //é obrigatorio começar com I no começo de toda interface
+{
+    void FazerSom();//Regra obrigatoria do "contrato"
+    void Comer();//Regra obrigatoria do "contrato"
+}
+
+class Cachorro : IAnimal//Implementa os métodos de IAnimal com comportamento próprio(é obrigatorio ter as regras/comportamento)
+{
+    public void Comer()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FazerSom()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Gato : IAnimal//Implementa o mesmo contrato, mas com outra lógica e outro animal
+{
+    public void FazerSom()//Regra/comportamento
+    {
+        Console.WriteLine("Gato");//ação do comportamento
+        Console.WriteLine("Miaaauu!");//ação do comportamento
+    }
+    public void Comer()//Regra/comportamento
+    {
+        Console.WriteLine("mastigando... comendo ração");//ação do comportamento
+    }
+}
+
+
+//fato curioso
+//ah interface é parecida com uma heranca mas tem a diferenca que quando é vinculada a uma classe ela é obrigatoria ter as regras q ela tem e alem disso a interface te entrega oque fazer menos como fazer já a heranca não vc herda tudo já feito.
